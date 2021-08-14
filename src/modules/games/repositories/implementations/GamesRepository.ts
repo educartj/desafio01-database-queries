c7cbf9c120b4154878485d2ed5db0425ceb12193
educartj <educartj@gmail.com>
2021-08-14 00:28:56 -0300

    Test - consultas no banco de dados com o TypeORM

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,17 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder('games').where('LOWER(games.title) LIKE LOWER(:title)', { title: `%${param}%` }).getMany()
+    // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT count(*) FROM games'); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder('games').relation(Game, 'users').of(id).printSql().loadMany()
+    // Complete usando query builder
   }
 }
